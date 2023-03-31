e130545577f968d4249c330444f6030e48bda8fa
david-vardanov <d.vardanov@gmail.com>
2023-03-31 18:12:28 +0400

    minor change 2

--- app.js
+++ app.js
@@ -85,8 +85,8 @@ app.use(handleError);
 
 // Certificate
 const options = {
-  cert: fs.readFileSync('/root/agd/cert.pem'),
-  key: fs.readFileSync('/root/agd/key.pem')
+  cert: fs.readFileSync('/root/cert.pem'),
+  key: fs.readFileSync('/root/key.pem')
 };
 
 // Create HTTPS server
