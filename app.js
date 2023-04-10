1cbc57a856f445dab36974508c9a46462b26dc16
david-vardanov <d.vardanov@gmail.com>
2023-04-10 10:57:25 +0400

    server change 2

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use(handleError);
 const credentials = { key: privateKey, cert: certificate, ca: ca };
 const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(process.env.PORT, () => {
+httpsServer.listen(process.env.SPORT, () => {
   console.log('HTTPS Server running on port' + process.env.PORT);
 });
 
