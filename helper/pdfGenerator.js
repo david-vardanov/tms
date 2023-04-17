f3e775ab87d8dc98a3474240501d958623253536
david-vardanov <d.vardanov@gmail.com>
2023-04-17 18:16:24 +0400

    working on BC 02-ff

--- helper/pdfGenerator.js
+++ helper/pdfGenerator.js
@@ -6,26 +6,19 @@ function generateBrokerCarrierAgreement(carrier) {
   const pdfStream = new stream.PassThrough();
 
   // Write PDF content
-  // ... (same content as before)
+  doc.fontSize(14);
+  doc.text('Broker-Carrier Agreement', { align: 'center' });
+  doc.moveDown();
 
-  // Return PDF stream
-  doc.pipe(pdfStream);
-  doc.end();
-  return pdfStream;
-}
+  doc.fontSize(12);
+  doc.text(`This agreement is made and entered into by and between the following parties:`);
+  doc.moveDown();
+  doc.text(`Carrier Name: ${carrier.name}`);
+  doc.text(`Carrier MC Number: ${carrier.mcNumber}`);
+  // Add more carrier details as needed
 
-module.exports = {
-  generateBrokerCarrierAgreement,
-};
-const PDFDocument = require('pdfkit');
-const stream = require('stream');
-
-function generateBrokerCarrierAgreement(carrier) {
-  const doc = new PDFDocument();
-  const pdfStream = new stream.PassThrough();
-
-  // Write PDF content
-  // ... (same content as before)
+  doc.moveDown();
+  doc.text(`[Place the legal text of the agreement here. You can add dynamic data using the carrier object.]`);
 
   // Return PDF stream
   doc.pipe(pdfStream);
