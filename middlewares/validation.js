3581dae85f4d5fa17b38bd90555c1e59f0f868be
david-vardanov <d.vardanov@gmail.com>
2023-04-18 12:17:47 +0400

    updating validation and adding flash -f

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -48,6 +48,15 @@ const loginSchema = Joi.object({
     next();
 };
 
+
+  const validateLogin = (req, res, next) => {
+    const { error } = loginSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      const errors = error.details.map((err) => err.message);
+      return res.status(400).json({ success: false, errors });
+    }
+    next();
+  };
   
 
 
